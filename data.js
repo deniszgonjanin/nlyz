9ceb0c121be0c67dbc2c132c9a70833cfd561da5
@deniszgonjanin <deniszgonjanin@gmail.com>
2011-09-18 12:41:56 -0400

    Data model

--- data.js
+++ data.js
@@ -10,8 +10,9 @@ redis.on('error', function(err){
 //Data model
 //[key]											[value]
 //----------------------------------
-//<link>											the unshortened link
-//<link>_
+//counter										the number of links (highest index)
+//<link_index>							{short_link, original_link, time_created}
+//<link_index>_analyze			{IP, browser, OS, time, referer}
 
 
 exports.someFunction = function(callback){
