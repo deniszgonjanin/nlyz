026f3673c601c4f6d9cfcf80db598b1871b5d647
@deniszgonjanin <deniszgonjanin@gmail.com>
2011-09-18 13:20:19 -0400

    Data layer, initial implementation.

--- data.js
+++ data.js
@@ -12,9 +12,34 @@ redis.on('error', function(err){
 //----------------------------------
 //counter										the number of links (highest index)
 //<link_index>							{short_link, original_link, time_created}
-//<link_index>_analyze			{IP, browser, OS, time, referer}
+//<link_index>_analyze			LIST OF: {IP, browser, OS, time, referer}
 
 
-exports.someFunction = function(callback){
-	
+exports.saveLink = function(linkIndex, linkObject, callback){
+	redis.set(linkIndex, linkObject);
+};
+
+exports.saveLinkHit = function(linkIndex, linkHitObject, callback){
+	var analyzeKey = linkIndex + "_analyze";
+	redis.lpush(analyzeKey, linkHitObject);
+};
+
+exports.getLink = function(linkIndex, callback){
+	redis.get(linkIndex, function(err, reply){
+		if (err) return callback(err);
+		
+		var linkObject = JSON.parse(reply.toString());
+		callback(null, linkObject);
+	});
+};
+
+exports.getLinkAnalytics = function(linkIndex, callback){
+	var analyzeKey = linkIndex + '_analyze';
+	redis.llen(analyzeKey, function(err, reply){
+		var length = parseInt(reply.toString());
+		
+		redis.lrange(analyzeKey, 0, length, function(err, reply){
+			var list = reply;
+		});
+	});
 };
