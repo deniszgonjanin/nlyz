b374e8faf6ccc3c06e9a461e5ed2dad535ee57b3
@deniszgonjanin <deniszgonjanin@gmail.com>
2011-09-19 12:23:38 -0400

    Trying port 80

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/:link_id', function(req, res){
 	
 });
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 /** Bellow is real time dashboard stuff **/
