c0f8ab328ec9cfeb91ccdabb96df9db93d55c077
@deniszgonjanin <deniszgonjanin@gmail.com>
2011-09-21 16:11:28 -0400

    New release to no.de

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-app.listen(3000);
+app.listen(80);
 var nowjs = require('now');
 var everyone = nowjs.initialize(app);
 
